248372dc5b5024f81453c944e6a80d3b3d77fe51
Jackie Chung <jackiechung1214@gmail.com>
2022-06-25 17:40:03 +0800

    4.5 批次爬取HTML型態的網頁資料

--- yahoo_scraper.py
+++ yahoo_scraper.py
@@ -8,9 +8,33 @@ soup=BeautifulSoup(response.text,'lxml')
 date=soup.find('time').get('datatime')
 
 rows=soup.find_all('div',{'Bgc(#fff) table-row D(f) H(48px) Ai(c) Bgc(#e7f3ff):h Fz(16px) Px(12px) Bxz(bb) Bdbs(s) Bdbw(1px) Bdbc($bd-primary-divider)'})
+
+result=[]
 for row in rows:
     company=row.find('div',{'class':'Lh(20px) Fw(600) Fz(16px) Ell'}).getText()
     price=row.find_all('div',{'class':'Fxg(1) Fxs(1) Fxb(0%) Ta(end) Mend($m-table-cell-space) Mend(0):lc Miw(68px)'})[0].getText()
-    status=row.find_all('div',{'class':'Fxg(1) Fxs(1) Fxb(0%) Ta(end) Mend($m-table-cell-space) Mend(0):lc Miw(74px)'})[0].getText()
-    percentage=row.find_all('div',{'class':'Fxg(1) Fxs(1) Fxb(0%) Ta(end) Mend($m-table-cell-space) Mend(0):lc Miw(74px)'})[1].getText()
-    print(date, company,price,status,percentage)
+
+    #由於yahoo股市漲跌是用顏色標記,故要先找到漲跌標記,再進行資料加工
+    status_element=row.find_all('div',{'class':'Fxg(1) Fxs(1) Fxb(0%) Ta(end) Mend($m-table-cell-space) Mend(0):lc Miw(74px)'})[0]
+    status_class=status_element.find('span').get('class')
+    status=''
+    if 'C($c-trend-down)' in status_class:
+        status='▼'+status_element.getText()
+    elif 'C($c-trend-up)' in status_class:
+        status='▲'+status_element.getText()
+    else:
+        status=status_element.getText()
+
+    percentage_element=row.find_all('div',{'class':'Fxg(1) Fxs(1) Fxb(0%) Ta(end) Mend($m-table-cell-space) Mend(0):lc Miw(74px)'})[1]
+    percentage_class=percentage_element.find('span').get('class')
+    percentage=''
+    if 'C($c-trend-down)' in percentage_class:
+        percentage='▼'+percentage_element.getText()
+    elif 'C($c-trend-up)' in percentage_class:
+        percentage='▲'+percentage_element.getText()
+    else:
+        percentage=percentage_element.getText()
+
+    result.append([date, company,price,status,percentage])
+    
+print(result)
